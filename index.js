0a42b3fcaf16c3c2af6b82405c584155f2de9d83
Caleb Kiptoo <vincentcalebkip@gmail.com>
2024-11-21 11:57:50 +0300

    array functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+function destructivelyPrependCat(name="Ralph"){
+    cats.unshift(name)
+    console.log(cats)
+
+}
+function destructivelyAppendCat(name){
+    cats.push(name)
+    console.log(cats)
+
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    console.log(cats)
+
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    console.log(cats)
+
+}
+function appendCat(name){
+    const cat = [...cats,name]
+    return cat
+
+}
+function prependCat(name){
+    const cat = [name,...cats]
+    return cat
+
+}
+function removeLastCat(){
+    const cat = cats.slice(0,-1)
+    return cat
+
+}
+function removeFirstCat(){
+    const cat = cats.slice(1)
+    return cat
+
+}
